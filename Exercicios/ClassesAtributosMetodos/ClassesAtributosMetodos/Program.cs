c5e6539eef6274214fb47e6290cdcd9647b7726b
Eduardo Coutinho Idalgo <104573566+idaaalgo@users.noreply.github.com>
2024-10-29 13:25:05 -0300

    Add files via upload

---
+++ Exercicios/ClassesAtributosMetodos/ClassesAtributosMetodos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassesAtributosMetodos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Entre a largura e altura (respectivamente) do retângulo: ");
+            double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Retangulo retangulo = new Retangulo(largura, altura);
+            Console.WriteLine($"AREA = {retangulo.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERIMETRO = {retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Exercicios/ClassesAtributosMetodos/ClassesAtributosMetodos/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassesAtributosMetodos
+{
+    internal class Retangulo
+    {
+        private double Largura { get; set; }
+        private double Altura { get; set; }
+
+        public Retangulo()
+        {
+        }
+
+        public Retangulo(double largura, double altura)
+        {
+            Largura = largura;
+            Altura = altura;
+        }
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Altura * Altura) + (Largura * Largura));
+        }
+    }
+}
