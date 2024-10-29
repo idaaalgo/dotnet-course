c46dfe878d880f6f6f60fd8e752ef797feed89a2
Eduardo Coutinho Idalgo <104573566+idaaalgo@users.noreply.github.com>
2024-10-29 13:32:08 -0300

    Add files via upload

---
+++ OperadoresLogico/OperadoresLogico/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OperadoresLogico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 4 != 5; // True
+            bool c2 = 2 > 3 && 4 != 5; // False
+            bool c3 = 2 > 3 || 4 != 5; // True
+            bool c4 = !(2 > 3) && 4 != 5; // True
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+            Console.WriteLine("----------------");
+
+            /*
+             * Ordem Precedência
+             * ! > && > ||
+             */
+
+            bool c5 = 10 < 5; // False
+            bool c6 = c3 || c4 && c5; // True
+            
+            Console.WriteLine(c5);
+            Console.WriteLine(c6);
+        }
+    }
+}
---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Hello World";
+            Console.WriteLine(texto);
+        }
+    }
+}
