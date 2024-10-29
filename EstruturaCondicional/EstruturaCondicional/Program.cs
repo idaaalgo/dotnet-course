59ea333d3f04bc05d4d5c6f416cbc4d04f7daf73
Eduardo Coutinho Idalgo <104573566+idaaalgo@users.noreply.github.com>
2024-10-29 13:28:40 -0300

    Add files via upload

---
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Que horas são?");
+            //int horaDoDia = int.Parse(Console.ReadLine());
+
+            //if (horaDoDia < 12)
+            //{
+            //    Console.WriteLine("Bom dia!");
+            //}
+            //else if (horaDoDia < 18)
+            //{
+            //    Console.WriteLine("Boa tarde!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Boa noite!");
+            //}
+
+            //Console.WriteLine("Digite um número: ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x < 0)
+            //{
+            //    Console.WriteLine("NEGATIVO");
+            //} 
+            //else
+            //{
+            //    Console.WriteLine("NÃO NEGATIVO");
+            //}
+
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine($"{num} é par");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{num} é ímpar");
+            //}
+
+            //string[] valores = Console.ReadLine().Split(' ');
+            //int a = int.Parse(valores[0]);
+            //int b = int.Parse(valores[1]);
+
+            //if (a % b == 0 || b % a == 0)
+            //{
+            //    Console.WriteLine("São multiplos");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não são multiplos");
+            //}
+
+            //string[] valores = Console.ReadLine().Split(' ');
+            //int horaInicial = int.Parse(valores[0]);
+            //int horaFinal = int.Parse(valores[1]);
+            //int duracao;
+            //if (horaInicial < horaFinal)
+            //{
+            //    duracao = horaFinal - horaInicial;
+            //}
+            //else
+            //{
+            //    duracao = 24 - horaInicial + horaFinal;
+            //}
+            //Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+
+            //string[] valores = Console.ReadLine().Split(' ');
+            //int codigo = int.Parse(valores[0]);
+            //int quantidade = int.Parse(valores[1]);
+            //double valorTotal = 0.0;
+            //if (codigo == 1)
+            //{
+            //    valorTotal = quantidade * 4.0;
+            //}
+            //else if (codigo == 2)
+            //{
+            //    valorTotal = quantidade * 4.5;
+            //}
+            //else if (codigo == 3)
+            //{
+            //    valorTotal = (quantidade * 5.0);
+            //}
+            //else if (codigo == 4)
+            //{
+            //    valorTotal = (quantidade * 2.0);
+            //}
+            //else if (codigo == 5)
+            //{
+            //    valorTotal = (quantidade * 1.5);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Opção inválida");
+            //}
+            //Console.WriteLine($"Total: {valorTotal.ToString("C")}");
+
+            //double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //if (x >= 0 && x <= 25)
+            //{
+            //    Console.WriteLine("Intervalo [0,25]");
+            //}
+            //else if (x > 25 && x <= 50){
+            //    Console.WriteLine("Intervalo (25,50]");
+            //}
+            //else if (x > 50 && x <= 100)
+            //{
+            //    Console.WriteLine("Intervalo(75, 100]");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Fora de intervalo");
+            //}
+        }
+    }
+}
---
+++ For/For/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace For
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você quer digitar? ");
+            int n = int.Parse(Console.ReadLine());
+            int soma = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Valor #{i}: ");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
